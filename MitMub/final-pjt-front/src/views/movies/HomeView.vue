c4485875cf292c617ad371882074da0b9b9d596e
recorror <itgwd9701@gamil.com>
2022-11-17 21:57:09 +0900

    1117 / 일과 중 프론트 작업

--- MitMub/final-pjt-front/src/views/movies/HomeView.vue
+++ MitMub/final-pjt-front/src/views/movies/HomeView.vue
@@ -1,76 +1,81 @@
 <template>
-    <div>
-        <header>
-            <div class="d-grid gap-2 d-md-flex justify-content-md-center mb-4 mt-5">
-                <input type="radio" class="btn-check" name="options" id="option1" autocomplete="off" checked @click="popularMovies">
-                <label class="btn btn-outline-secondary me-md-1" for="option1">인기순</label>
+  <div>
+    <header>
+      <h2>🔥Hot Movies</h2>
+      <div class="d-grid gap-2 d-md-flex justify-content-md-center mb-4 mt-5">
 
-                <input type="radio" class="btn-check" name="options" id="option2" autocomplete="off" @click="highRateMovies">
-                <label class="btn btn-outline-secondary me-md-1" for="option2">평점순</label>
+        <input type="radio" class="btn-check" name="options" id="option1" autocomplete="off" checked @click="popularMovies">
+        <label class="btn btn-outline-secondary me-md-1" for="option1">인기순</label>
 
-                <input type="radio" class="btn-check" name="options" id="option3" autocomplete="off" @click="newMovies">
-                <label class="btn btn-outline-secondary me-md-1" for="option3">최신순</label>
+        <input type="radio" class="btn-check" name="options" id="option2" autocomplete="off" @click="highRateMovies">
+        <label class="btn btn-outline-secondary me-md-1" for="option2">평점순</label>
 
-                <input type="radio" class="btn-check" name="options" id="option4" autocomplete="off">
-                <label class="btn btn-outline-secondary me-md-1" for="option4">상영예정작</label>
+        <input type="radio" class="btn-check" name="options" id="option3" autocomplete="off" @click="newMovies">
+        <label class="btn btn-outline-secondary me-md-1" for="option3">최신순</label>
 
-                <input type="radio" class="btn-check" name="options" id="option5" autocomplete="off">
-                <label class="btn btn-outline-secondary me-md-1" for="option5">킬링타임용</label>
+        <input type="radio" class="btn-check" name="options" id="option4" autocomplete="off">
+        <label class="btn btn-outline-secondary me-md-1" for="option4">상영예정작</label>
 
+        <input type="radio" class="btn-check" name="options" id="option5" autocomplete="off">
+        <label class="btn btn-outline-secondary me-md-1" for="option5">킬링타임용</label>
 
-            </div>
-         
-        </header>
-        <div class="popular-list row row-cols-1 row-cols-md-5 gy-3">
+      </div>
+    </header>
+    <div class="card p-0 m-0">
+      <div class="popular-list card-body row row-cols-3 gy-3">
         <MovieCard
-            v-for="(movie, idx) in nowMovies"
-            :key="idx"
-            :movie="movie"/>
-        </div>
+          v-for="(movie, idx) in movies.slice(0,10)"
+          :key="idx"
+          :movie="movie"
+        />
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-
+import { mapGetters } from 'vuex'
 import MovieCard from '@/components/movies/MovieCard'
 
 export default {
-    name: 'HomeView',
-    data() {
-        return {
-            nowMovies: [],
-        }
-    },
-    components: {
-        MovieCard,
-    },
-    computed: {
-        
-    isLogin() {
-        return this.$store.getters.isLogin
+  name: 'HomeView',
+  data() {
+    return {
+      nowMovies: [],
     }
-    },
-    created() {
-        if (this.isLogin === true) {
-            this.popularMovies()
-        } else {
-            this.$router.push({ name: 'LogInView'})
-        }
-        
-    },
-    methods: {
-        popularMovies() {
-            this.nowMovies = this.$store.getters.movies
-            console.log(this.$store.getters.movies)
-        },
-        highRateMovies() {
-             this.nowMovies = this.$store.getters.highRateMovies
-        },
-        newMovies() {
-             this.nowMovies = this.$store.getters.newMovies
-        },
-        
+  },
+  components: {
+    MovieCard,
+  },
+  computed: {
+    ...mapGetters([
+      'movies',
+  ]), 
+  isLogin() {
+    return this.$store.getters.isLogin
+  }
+  },
+  created() {
+    if (this.isLogin === true) {
+      this.popularMovies()
+    } else {
+      this.$router.push({ name: 'LogInView'})
     }
+      
+  },
+  methods: {
+    popularMovies() {
+      this.nowMovies = this.$store.getters.movies
+      console.log(this.$store.getters.movies)
+    },
+    highRateMovies() {
+      this.nowMovies = this.$store.getters.highRateMovies
+    },
+    newMovies() {
+      this.nowMovies = this.$store.getters.newMovies
+    },
+      
+  }
 }
 </script>
 
